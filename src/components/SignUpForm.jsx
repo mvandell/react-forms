1d609214dadb8661da9c09b46cee38c02264f7e0
Marisa <marisavandellos@gmail.com>
2023-09-14 18:19:47 -0700

    working

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -4,14 +4,24 @@ export default function SignUpForm() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
+    
     async function handleSubmit(event) {
         event.preventDefault();
-        console.log("hello world")
+        
+        try {
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+                method: "POST",
+                
+            })
+        } catch (error) {
+            setError(error.message);
+        }
     };
 
     return (
         <>
         <h2>Sign Up</h2>
+        {error && <p>{error}</p>}
         <form onSubmit={handleSubmit}>
             <label>
                 Username: <input value={username} onChange={(event) => {setUsername(event.target.value)}}/>
