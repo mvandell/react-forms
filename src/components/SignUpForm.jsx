ae88a684b1ee86a5e516a518f65d343e432f8ce7
Marisa <marisavandellos@gmail.com>
2023-09-17 11:06:06 -0700

    sign up component done

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -11,8 +11,12 @@ export default function SignUpForm() {
         try {
             const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
                 method: "POST",
-                
-            })
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({username, password})
+            });
+            const result = await response.json();
+            console.log(result);
+
         } catch (error) {
             setError(error.message);
         }
